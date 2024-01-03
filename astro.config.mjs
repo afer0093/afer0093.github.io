c5b4db0c94596ac7c298df4f0e16fff7c104bf9c
afer0093 <peanuts1909@gmail.com>
2024-01-03 19:22:26 +0900

    new file:   .giyhub/workflows/deploy.yml 	modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://afer0093.github.io',
+});
